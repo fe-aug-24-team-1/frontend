b2bf525b3b4d12c617724e6ca86cbc71cc17346a
EustasTheMonk <kononenko.dima2001@gmail.com>
2024-11-04 21:27:54 +0200

    86c0w8m89: implemented product card

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styles from './ProductCard.module.scss';
+import { TypePhone } from '@/types/TypePhone.ts';
+import { Button } from '@/components/Button';
+import { FavoriteButton } from '@/components/FavoriteButton/FavoriteButton.tsx';
+import { PhoneCardSpecs } from '@/components/PhoneCardSpecs/PhoneCardSpecs.tsx';
+
+interface Props {
+  product: TypePhone;
+}
+
+export const ProductCard: React.FC<Props> = ({ product }) => {
+  return (
+    <div className={styles.product}>
+      <img
+        className={styles[`product__image`]}
+        alt={product.name}
+        src={product.images[0]}
+      />
+
+      <p className={styles[`product__name`]}>{product.name}</p>
+
+      <div
+        title="Product prices"
+        className={styles[`product__prices`]}>
+        <span className={styles[`product__prices--current`]}>
+          {`$${product.priceDiscount}`}
+        </span>
+        <span className={styles[`product__prices--regular`]}>
+          {`$${product.priceRegular}`}
+        </span>
+      </div>
+
+      <div className={styles[`product__decorative-line`]}></div>
+
+      <div className={styles[`product__specs`]}>
+        {/*{product.category === 'phone' && <PhoneCardSpecs product={product} />}*/}
+        <PhoneCardSpecs product={product} />
+      </div>
+
+      <div className={styles[`product__buttons-container`]}>
+        <Button />
+
+        <FavoriteButton />
+      </div>
+    </div>
+  );
+};
