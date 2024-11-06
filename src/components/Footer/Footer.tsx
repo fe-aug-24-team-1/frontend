2439743aa8d09fb88c7a2f712127a2ce3ca6a2c1
Sukulynt <maxlinnyk100@gmail.com>
2024-11-06 13:15:39 +0200

    86c0w8m7q: fixed footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,66 +1,55 @@
-import styles from './Footer.module.scss';
-import Logo from '../../../public/logo.svg';
-import { Link } from 'react-router-dom';
-import { SliderButton } from '../SliderButton';
-import Arrow from '../../assets/images/icons/chevron-up.svg';
+import style from './Footer.module.scss';
+import logo from '../../../public/logo.svg';
+import { NavLink } from 'react-router-dom';
 
-type NavLink = { title: string; path: string };
-
-const links: NavLink[] = [
-  { title: 'Github', path: '/' },
-  { title: 'Contacts', path: '/' },
-  { title: 'Rights', path: '/' },
+const navigation = [
+  {
+    href: 'https://github.com/Nazarin565/react_phone-catalog/',
+    name: 'Github',
+  },
+  {
+    href: 'https://github.com/Nazarin565',
+    name: 'Contacts',
+  },
+  {
+    href: 'https://github.com/Nazarin565',
+    name: 'Rights',
+  },
 ];
-
-const scrollToTop = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
-};
 
 export const Footer = () => {
   return (
-    <footer className={styles.Footer}>
-      <div className={styles.Footer__content}>
-        <Link
-          to="/"
-          className={styles.Footer__logo}>
-          <img
-            src={Logo}
-            alt="Logo"
-          />
-        </Link>
-
-        <nav className={styles.Footer__nav}>
-          <ul className={styles.Footer__list}>
-            {links.map((link) => (
-              <li
-                key={link.title}
-                className={styles.Footer__item}>
-                <a
-                  href={link.path}
-                  target="_blank"
-                  className={styles.Footer__link}
-                  rel="noreferrer">
-                  {link.title}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-
-        <div className={styles.Footer__backTop}>
-          <span>Back to top</span>
-          <SliderButton onClick={scrollToTop}>
-            <img
-              src={Arrow}
-              alt="Scroll to top"
-              className={styles.Footer__icon}
-            />
-          </SliderButton>
-        </div>
-      </div>
+    <footer className={style.footer}>
+      <a
+        href="#"
+        className={style.logo__link}>
+        <img
+          src={logo}
+          className={style.logo}
+        />
+      </a>
+      <nav className={style.nav}>
+        <ul className={style.nav__list}>
+          {navigation.map(({ href, name }) => (
+            <li
+              key={name}
+              className={style.nav__item}>
+              <NavLink
+                to={href}
+                target="_blank"
+                className={style.nav__link}>
+                {name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <button
+        className={style.backToTop}
+        onClick={() => window.scrollTo({ top: 0 })}>
+        <p className={style.backToTop__text}>Back to top</p>
+        <div className={style.backToTop__button} />
+      </button>
     </footer>
   );
 };
