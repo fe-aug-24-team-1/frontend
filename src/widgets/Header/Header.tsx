4deb5e3d665ae669735ec31492011cf328cf59d4
Rostyslav <rostislavkpi@gmail.com>
2024-11-07 13:08:00 +0200

    refactored header component

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,24 +1,46 @@
 import { FC, useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { IoSunnyOutline, IoMoonOutline } from 'react-icons/io5';
+import cn from 'classnames';
+import i18next from 'i18next';
+
+import { Lang } from '@/types/Lang';
+
+import {
+  ThemeDispatchContext,
+  ThemeStateContext,
+} from '@/app/providers/ThemeProvider';
+import { Icon } from '@/components/icon/Icon';
 
 import logo from '/logo.svg';
 
-import './Header.scss';
-import cn from 'classnames';
-import { ThemeDispatchContext } from '@/app/providers/ThemeProvider';
-import i18next from 'i18next';
-import { Lang } from '@/types/Lang';
-import { Icon } from '@/components/icon/Icon';
+import styles from './Header.module.scss';
+
+const navItems = [
+  { path: '/', name: 'home' },
+  { path: 'phones', name: 'phones' },
+  { path: 'tablets', name: 'tablets' },
+  { path: 'accessories', name: 'accessories' },
+];
 
 export const Header: FC = () => {
   const toggleTheme = useContext(ThemeDispatchContext);
+  const { theme } = useContext(ThemeStateContext);
 
   const { t } = useTranslation();
 
   const getLinkClass = ({ isActive }: { isActive: boolean }) =>
-    cn('header__nav--item_link', {
-      'header__nav--item_link--active': isActive,
+    cn(styles.nav__link, {
+      [styles['nav__link--active']]: isActive,
+    });
+
+  const getActiveIcon = (
+    { isActive }: { isActive: boolean },
+    iconName: string
+  ) =>
+    cn(styles.header__iconContainer, styles[`header__${iconName}`], {
+      [styles['header__iconContainer--active']]: isActive,
     });
 
   const changeLang = () => {
@@ -28,89 +50,76 @@ export const Header: FC = () => {
   };
 
   return (
-    <header className="header">
-      <div className="top-bar">
-        <div className="top-bar_left">
-          <div className="header__logo">
+    <>
+      <header className={styles.header}>
+        <div className={styles.header__left}>
+          <NavLink
+            to="/"
+            className={styles.logo__link}>
             <img
               src={logo}
               alt="Nice gadgets logo"
             />
-          </div>
+          </NavLink>
 
-          <nav className="header__nav">
-            <ul className="header__nav--list">
-              <li className="header__nav--item">
-                <NavLink
-                  to="/"
-                  className={getLinkClass}>
-                  {t('header.nav.home')}
-                </NavLink>
-              </li>
-
-              <li>
-                <NavLink
-                  to="/phones"
-                  className={getLinkClass}>
-                  {t('header.nav.phones')}
-                </NavLink>
-              </li>
-
-              <li>
-                <NavLink
-                  to="/tablets"
-                  className={getLinkClass}>
-                  {t('header.nav.tablets')}
-                </NavLink>
-              </li>
-
-              <li>
-                <NavLink
-                  to="/accessories"
-                  className={getLinkClass}>
-                  {t('header.nav.accessories')}
-                </NavLink>
-              </li>
+          <nav className={styles.nav}>
+            <ul className={styles.nav__list}>
+              {navItems.map(({ path, name }) => (
+                <li
+                  key={name}
+                  className={styles.nav__item}>
+                  <NavLink
+                    to={path}
+                    className={getLinkClass}>
+                    {t(`header.nav.${name}`)}
+                  </NavLink>
+                </li>
+              ))}
             </ul>
           </nav>
         </div>
 
-        <div className="top-bar_right">
-          <div className="icon">
-            <div className="header__theme">
-              <div className="header__theme-button">
-                <img
-                  alt="Theme"
-                  className="header__theme-button-img"
+        <div className={styles.header__right}>
+          <div
+            className={`${styles.header__iconContainer} ${styles.header__iconTheme}`}>
+            <div className={styles.header__iconCart}>
+              {theme === 'dark' ? (
+                <IoSunnyOutline
+                  className={styles.header__icon}
                   onClick={toggleTheme}
                 />
-              </div>
+              ) : (
+                <IoMoonOutline
+                  className={styles.header__icon}
+                  onClick={toggleTheme}
+                />
+              )}
             </div>
-
-            <div
-              className="header__lang"
-              onClick={changeLang}>
-              <div className="header__lang-button">{t('header.lang')}</div>
-            </div>
-
-            <NavLink to="/favorites">
-              <Icon.Favorites className="icon--favourites_img" />
-            </NavLink>
-
-            <NavLink
-              to="/cart"
-              className="header__icons--cart">
-              <Icon.ShoppingBag className="icon--cart_img" />
-            </NavLink>
-
-            <a
-              href="#burger_menu"
-              className="burger_menu_icon">
-              <Icon.Menu className="burger_menu_icon_img" />
-            </a>
           </div>
+
+          <div
+            className={`${styles.header__iconContainer} ${styles.header__iconLang}`}
+            onClick={changeLang}>
+            <div className={styles.header__icon}>{t('header.lang')}</div>
+          </div>
+
+          <NavLink
+            to="/favorites"
+            className={({ isActive }) =>
+              getActiveIcon({ isActive }, 'iconLike')
+            }>
+            <Icon.Favorites className={styles.header__icon} />
+          </NavLink>
+
+          <NavLink
+            to="/cart"
+            className={({ isActive }) =>
+              getActiveIcon({ isActive }, 'iconCart')
+            }>
+            <Icon.ShoppingBag className={styles.header__icon} />
+          </NavLink>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
