b215eff776d81e6b0f15d70e62e3cf7d2c6d2cee
Sukulynt <98977935+Sukulynt@users.noreply.github.com>
2024-11-08 10:54:34 +0200

    Update Footer.tsx
    fixed an issue

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,52 +4,51 @@ import { NavLink } from 'react-router-dom';
 
 const navigation = [
   {
-    href: 'https://github.com/Nazarin565/react_phone-catalog/',
+    href: 'https://github.com/Sukulynt/react_product-categories/',
     name: 'Github',
   },
   {
-    href: 'https://github.com/Nazarin565',
+    href: 'https://github.com/Sukulynt',
     name: 'Contacts',
   },
   {
-    href: 'https://github.com/Nazarin565',
+    href: 'https://github.com/Sukulynt',
     name: 'Rights',
   },
 ];
 
-export const Footer = () => {
-  return (
-    <footer className={style.footer}>
-      <a
-        href="#"
-        className={style.logo__link}>
-        <img
-          src={logo}
-          className={style.logo}
-        />
-      </a>
-      <nav className={style.nav}>
-        <ul className={style.nav__list}>
-          {navigation.map(({ href, name }) => (
-            <li
-              key={name}
-              className={style.nav__item}>
-              <NavLink
-                to={href}
-                target="_blank"
-                className={style.nav__link}>
-                {name}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <button
-        className={style.backToTop}
-        onClick={() => window.scrollTo({ top: 0 })}>
-        <p className={style.backToTop__text}>Back to top</p>
-        <div className={style.backToTop__button} />
-      </button>
-    </footer>
-  );
-};
+export const Footer = () => (
+  <footer className={style.footer}>
+    <a
+      href="#"
+      className={style.logo__link}>
+      <img
+        src={logo}
+        alt="Logo"
+        className={style.logo}
+      />
+    </a>
+    <nav className={style.nav}>
+      <ul className={style.nav__list}>
+        {navigation.map(({ href, name }) => (
+          <li
+            key={name}
+            className={style.nav__item}>
+            <NavLink
+              to={href}
+              target="_blank"
+              className={style.nav__link}>
+              {name}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+    <button
+      className={style.backToTop}
+      onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+      <p className={style.backToTop__text}>Back to top</p>
+      <div className={style.backToTop__button}></div>
+    </button>
+  </footer>
+);
