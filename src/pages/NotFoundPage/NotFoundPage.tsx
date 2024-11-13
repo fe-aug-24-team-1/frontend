6441f046956ef186ca814c470912c04188a542c4
EustasTheMonk <kononenko.dima2001@gmail.com>
2024-11-13 18:51:16 +0200

    adding Icon to button, fix notFoundPage text, fix styles, add ability to add classes to buttonCommon

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import notFoundPage from '/img/page-not-found.png';
 import style from './NotFoundPage.module.scss';
 import { useTranslation } from 'react-i18next';
+import { ButtonCommon } from '@/components/ButtonCommon';
 
 export const NotFoundPage = () => {
   const navigate = useNavigate();
@@ -11,12 +12,19 @@ export const NotFoundPage = () => {
   return (
     <div className={style.pageNotFound}>
       <h1 className={style.pageNotFound__title}>{t('notFoundPage.title')}</h1>
-      <button
-        type="button"
-        className={style.pageNotFound__button}
+      {/*<button*/}
+      {/*  type="button"*/}
+      {/*  className={style.pageNotFound__button}*/}
+      {/*  onClick={() => navigate('/home')}>*/}
+      {/*  {t('notFoundPage.backToHome')}*/}
+      {/*</button>*/}
+      <div
+        // className={style.pageNotFound__button__text}
         onClick={() => navigate('/home')}>
-        {t('notFoundPage.backToHome')}
-      </button>
+        <ButtonCommon className={style.pageNotFound__button__text}>
+          {t('notFoundPage.backToHome')}
+        </ButtonCommon>
+      </div>
       <img
         src={notFoundPage}
         alt="Empty Cart"
